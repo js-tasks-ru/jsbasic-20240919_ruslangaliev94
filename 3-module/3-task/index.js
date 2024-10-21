9a010e9dbf499356139bd3a59510d7e454fa08f5
ruslan <ruslangaliev94@gmail.com>
2024-10-21 17:08:47 +0300

    3-module-3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
-  // ваш код...
+    let camelize = str.split('-');
+    let camelized = camelize.map(function (item, index) {
+        if (index > 0) {
+            return item[0].toUpperCase() + item.slice(1).toLowerCase();
+        }
+        return item;
+
+    });
+
+    return camelized.join('');
 }
+
+
