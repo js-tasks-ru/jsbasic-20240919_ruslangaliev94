3864787eb29b938d3682ee0856ea8b580aa39d4a
Ruslan G <ruslangaliev94@gmail.com>
2024-10-31 17:39:54 +0300

    3-module 5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,18 @@
 function getMinMax(str) {
-  // ваш код...
+
+  let arr = str.split(' ');
+  let numbers = [];
+  let min;
+  let max;
+
+  arr.forEach(function (item) {
+    let num = parseFloat(item);
+    if (num === num) {
+      numbers.push(num);
+    }
+  });
+
+  min = Math.min(...numbers);
+  max = Math.max(...numbers);
+  return {min, max};
 }
