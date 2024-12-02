c903d2ee2f05b3bd89fd8280592a7c33af7e5484
Ruslan G <ruslangaliev94@gmail.com>
2024-12-02 23:30:45 +0300

    6-module 3-task

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,90 +1,89 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Carousel {
-    constructor(slides) {
-        this.slides = slides;
-        this.render();
-    }
+  constructor(slides) {
+    this.slides = slides;
+    this.currentSlide = 0; // Отслеживаем текущий слайд
+    this.render();
+    this.initCarousel();
+  }
 
-    render() {
-        this.elem = createElement(`
-            <div class="carousel">
-                <div class="carousel__arrow carousel__arrow_right">
-                    <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+  render() {
+    this.elem = createElement(`
+      <div class="carousel">
+        <div class="carousel__arrow carousel__arrow_right">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__arrow carousel__arrow_left">
+          <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__inner">
+          ${this.slides
+      .map(({name, price, image, id}) => `
+              <div class="carousel__slide" data-id="${id}">
+                <img src="/assets/images/carousel/${image}" class="carousel__img" alt="${name}">
+                <div class="carousel__caption">
+                  <span class="carousel__price">€${price.toFixed(2)}</span>
+                  <div class="carousel__title">${name}</div>
+                  <button type="button" class="carousel__button">
+                    <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+                  </button>
                 </div>
-                <div class="carousel__arrow carousel__arrow_left">
-                    <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
-                </div>
-                <div class="carousel__inner">
-                    ${this.slides
-            .map(({name, price, image, id}) =>
-                `
-                            <div class="carousel__slide" data-id="${id}">
-                                <img src="/assets/images/carousel/${image}" class="carousel__img" alt="${name}">
-                                <div class="carousel__caption">
-                                    <span class="carousel__price">€${(price).toFixed(2)}</span>
-                                    <div class="carousel__title">${name}</div>
-                                    <button type="button" class="carousel__button">
-                                        <img src="/assets/images/icons/plus-icon.svg" alt="icon">
-                                    </button>
-                                </div>
-                            </div>
-                        `).join('')}
-                </div>
-            </div>
-        `);
+              </div>
+            `).join('')}
+        </div>
+      </div>
+    `);
 
-        let arowLeft = this.elem.querySelector('.carousel__arrow_left');
-        let arowRight = this.elem.querySelector('.carousel__arrow_right');
-        let carouselInner = this.elem.querySelector('.carousel__inner');
-        let carouselSlide = this.elem.querySelectorAll('.carousel__slide');
+    // Добавляем обработчики на кнопки добавления продукта
+    this.addProductEventListeners();
+  }
 
-        let currentSlide = 0;
-
-        // Сохраняем ширину карусели в переменную
-        let carouselWidth = carouselInner.offsetWidth;
-
-        // Ждем немного перед получением ширины (если нужно больше времени для рендеринга)
-        setTimeout(() => {
-            carouselWidth = carouselInner.offsetWidth;
-            console.log(carouselWidth);
-        }, 0);
-
-        // Добавляем обработчик на клик по карусели
-        this.elem.addEventListener('click', (e) => {
-            if (e.target.classList.contains('carousel__arrow_left')) {
-                console.log('клик лево');
-                currentSlide--;
-                console.log(currentSlide * carouselWidth);
-                carouselInner.style.transform = `translateX(${-currentSlide * carouselWidth}px)`;
-
-                if (currentSlide <= 0) {
-                    arowLeft.style.display = 'none';
-                } else {
-                    arowLeft.style.display = 'block';
-                }
-
-                if (currentSlide < carouselSlide.length - 1) {
-                    arowRight.style.display = 'block';
-                }
-
-            } else if (e.target.classList.contains('carousel__arrow_right')) {
-                console.log('клик право');
-                currentSlide++;
-                carouselInner.style.transform = `translateX(${-currentSlide * carouselWidth}px)`;
-
-                if (currentSlide >= carouselSlide.length - 1) {
-                    arowRight.style.display = 'none';
-                } else {
-                    arowRight.style.display = 'block';
-                }
-                if (currentSlide > 0) {
-                    arowLeft.style.display = 'block';
-                }
-            }
-
-            console.log(currentSlide);
-            console.log(carouselWidth);
+  addProductEventListeners() {
+    const buttons = this.elem.querySelectorAll('.carousel__button');
+    buttons.forEach((button) => {
+      button.addEventListener('click', () => {
+        const slideId = button.closest('.carousel__slide').dataset.id;
+        const event = new CustomEvent('product-add', {
+          detail: slideId,
+          bubbles: true,
         });
-    }
+        this.elem.dispatchEvent(event);
+        console.log('тык', event.detail);
+      });
+    });
+  }
+
+  initCarousel() {
+    const arrowLeft = this.elem.querySelector('.carousel__arrow_left');
+    const arrowRight = this.elem.querySelector('.carousel__arrow_right');
+    const carouselInner = this.elem.querySelector('.carousel__inner');
+    const totalSlides = this.slides.length;
+
+
+    const updateCarousel = () => {
+      const offset = -this.currentSlide * carouselInner.offsetWidth;
+      carouselInner.style.transform = `translateX(${offset}px)`;
+
+      // Показываем/скрываем стрелки в зависимости от текущего слайда
+      arrowLeft.style.display = this.currentSlide === 0 ? 'none' : '';
+      arrowRight.style.display = this.currentSlide === totalSlides - 1 ? 'none' : '';
+
+
+      console.log(`${this.currentSlide + 1 } из ${totalSlides}`);
+    };
+
+    arrowLeft.addEventListener('click', () => {
+      this.currentSlide = Math.max(this.currentSlide - 1, 0);
+      updateCarousel();
+    });
+
+    arrowRight.addEventListener('click', () => {
+      this.currentSlide = Math.min(this.currentSlide + 1, totalSlides - 1);
+      updateCarousel();
+    });
+
+    // Обновляем карусель при первом рендере
+    updateCarousel();
+  }
 }
